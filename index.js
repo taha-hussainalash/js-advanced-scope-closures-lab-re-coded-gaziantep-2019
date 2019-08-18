a8591d927ae034e96a8cedbf1ccd77220f9d1d40
Taha HussainAlash <taha.benzma94@gmail.com>
2019-08-18 13:12:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ function produceDrivingRange( blockRange ) {
   return function ( startingBlock, endingBlock ) {
 
     let start = parseInt( startingBlock );
+   
     let end = parseInt( endingBlock );
+   
     let distanceToTravel = Math.abs( end - start );
+   
     let difference = blockRange - distanceToTravel;
 
     if ( difference > 0 ) {
       return `within range by ${difference}`
-    } else {
+    } 
+    
+    else {
       return `${Math.abs(difference)} blocks out of range`
     }
   }
